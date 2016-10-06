541135b05d3df2ae6c6b9291a6fae202583f5bd2
billyninja <joaoeduardocm@gmail.com>
2016-10-06 00:09:21 -0300

    introducing camera panning world x screen translation

--- main.go
+++ main.go
@@ -13,7 +13,31 @@ const (
 	tileSize            int32 = 32
 	cY                  int32 = winHeight / tileSize
 	cX                  int32 = winWidth / tileSize
+	WORLD_CELLS_X             = 500
+	WORLD_CELLS_Y             = 200
 )
+
+type Vector2d struct {
+	X int32
+	Y int32
+}
+
+type Camera struct {
+	P Vector2d
+}
+
+type AnimatedObj struct {
+	Position Vector2d
+	Action   [8]*sdl.Rect
+	Pose     uint8
+	PoseTick uint32
+}
+
+type StillObj struct {
+	Position  Vector2d
+	Source    *sdl.Rect
+	Collision uint8
+}
 
 var (
 	winTitle string = "Go-SDL2 Obj0"
@@ -22,9 +46,8 @@ var (
 	TREE     *sdl.Rect = &sdl.Rect{0, 32, tileSize, tileSize}
 	WALL     *sdl.Rect = &sdl.Rect{0, 64, tileSize, tileSize}
 	WOMAN    *sdl.Rect = &sdl.Rect{0, 128, tileSize, tileSize}
-)
 
-var (
+	// MAIN CHAR POSES AND ANIMATIONS
 	MAN_FRONT_R *sdl.Rect = &sdl.Rect{0, 0, tileSize, tileSize}
 	MAN_FRONT_N *sdl.Rect = &sdl.Rect{32, 0, tileSize, tileSize}
 	MAN_FRONT_L *sdl.Rect = &sdl.Rect{64, 0, tileSize, tileSize}
@@ -42,9 +65,7 @@ var (
 	MAN_WALK_LEFT  [8]*sdl.Rect = [8]*sdl.Rect{MAN_LEFT_N, MAN_LEFT_R, MAN_LEFT_N, MAN_LEFT_L}
 	MAN_WALK_RIGHT [8]*sdl.Rect = [8]*sdl.Rect{MAN_RIGHT_N, MAN_RIGHT_R, MAN_RIGHT_N, MAN_RIGHT_L}
 	MAN_WALK_BACK  [8]*sdl.Rect = [8]*sdl.Rect{MAN_BACK_N, MAN_BACK_R, MAN_BACK_N, MAN_BACK_L}
-)
 
-var (
 	EXPLOSION_S1 *sdl.Rect = &sdl.Rect{128, 0, tileSize, tileSize}
 	EXPLOSION_S2 *sdl.Rect = &sdl.Rect{128, 32, tileSize, tileSize}
 	EXPLOSION_S3 *sdl.Rect = &sdl.Rect{128, 64, tileSize, tileSize}
@@ -53,33 +74,21 @@ var (
 	EXPLOSION_A [8]*sdl.Rect = [8]*sdl.Rect{EXPLOSION_S1, EXPLOSION_S2, EXPLOSION_S3, EXPLOSION_S4}
 )
 
-type Vector2d struct {
-	X int32
-	Y int32
-}
+var (
+	Cam = Camera{
+		P: Vector2d{300, 300},
+	}
+	PC = AnimatedObj{
+		Position: Vector2d{Cam.P.X + 120, Cam.P.Y + 90},
+		Action:   MAN_WALK_FRONT,
+		Pose:     0,
+		PoseTick: 16,
+	}
 
-type AnimatedObj struct {
-	Position Vector2d
-	Action   [8]*sdl.Rect
-	Pose     uint8
-	PoseTick uint32
-}
-
-type StillObj struct {
-	Position  Vector2d
-	Source    *sdl.Rect
-	Collision uint8
-}
-
-var PC = AnimatedObj{
-	Position: Vector2d{0, 0},
-	Action:   MAN_WALK_FRONT,
-	Pose:     0,
-	PoseTick: 16,
-}
-
-var Obstacles []*StillObj
-var Explosions []*AnimatedObj
+	World      [WORLD_CELLS_X][WORLD_CELLS_Y]*sdl.Rect
+	Obstacles  []*StillObj
+	Explosions []*AnimatedObj
+)
 
 func checkCol(p1 Vector2d, p2 Vector2d) bool {
 	return (p1.X < p2.X+tileSize &&
@@ -95,15 +104,19 @@ func handleKeyEvent(key sdl.Keycode) {
 	case 1073741906:
 		PC.Action = MAN_WALK_BACK
 		np.Y -= 1
+		Cam.P.Y -= 1
 	case 1073741905:
 		PC.Action = MAN_WALK_FRONT
 		np.Y += 1
+		Cam.P.Y += 1
 	case 1073741904:
 		PC.Action = MAN_WALK_LEFT
 		np.X -= 1
+		Cam.P.X -= 1
 	case 1073741903:
 		PC.Action = MAN_WALK_RIGHT
 		np.X += 1
+		Cam.P.X += 1
 	}
 
 	if np.X == PC.Position.X && np.Y == PC.Position.Y {
@@ -120,6 +133,7 @@ func handleKeyEvent(key sdl.Keycode) {
 			}
 		}
 		PC.Position = np
+
 		PC.PoseTick -= 1
 		if PC.PoseTick == 0 {
 			PC.Pose = get_next_pose(PC.Action, PC.Pose)
@@ -149,6 +163,8 @@ func get_next_pose(action [8]*sdl.Rect, currPose uint8) uint8 {
 }
 
 func updateScene() {
+
+	// update explosions poses
 	for _, exps := range Explosions {
 		exps.PoseTick -= 1
 		if exps.PoseTick == 0 {
@@ -158,37 +174,56 @@ func updateScene() {
 	}
 }
 
+func worldToScreen(pos Vector2d, cam Camera) Vector2d {
+	return Vector2d{
+		X: pos.X - cam.P.X,
+		Y: pos.Y - cam.P.Y,
+	}
+}
+
 func renderScene(renderer *sdl.Renderer, ts *sdl.Texture, ss *sdl.Texture) {
+	var init int32 = 0
+	var Source *sdl.Rect
+
+	var offsetX, offsetY int32 = tileSize, tileSize
+
 	renderer.Clear()
+	renderer.SetDrawColor(0, 0, 255, 255)
 
-	var (
-		ys int32
-		xs int32
-	)
+	// Rendering the terrain
+	for winY := init; winY < winHeight; winY += offsetY {
+		for winX := init; winX < winWidth; winX += offsetX {
 
-	// Terrain
-	for ys = 0; ys < cY; ys++ {
-		for xs = 0; xs < cX; xs++ {
-			pos := &sdl.Rect{xs * tileSize, ys * tileSize, tileSize, tileSize}
-			renderer.Copy(ts, GRASS, pos)
+			offsetX = (tileSize - ((Cam.P.X + winX) % tileSize))
+			offsetY = (tileSize - ((Cam.P.Y + winY) % tileSize))
+
+			worldCellX := uint16((Cam.P.X + winX) / tileSize)
+			worldCellY := uint16((Cam.P.Y + winY) / tileSize)
+
+			gfx := World[worldCellX][worldCellY]
+
+			if offsetX != int32(tileSize) || offsetY != int32(tileSize) {
+				Source = &sdl.Rect{gfx.X + (tileSize - offsetX), gfx.Y + (tileSize - offsetY), offsetX, offsetY}
+			} else {
+				Source = gfx
+			}
+
+			screenPos := sdl.Rect{winX, winY, offsetX, offsetY}
+			renderer.Copy(ts, Source, &screenPos)
 		}
 	}
 
-	pos := &sdl.Rect{32, 0, tileSize, tileSize}
-	renderer.Copy(ss, WOMAN, pos)
+	// Rendering the PC
+	scrPoint := worldToScreen(PC.Position, Cam)
+	pos := sdl.Rect{scrPoint.X, scrPoint.Y, tileSize, tileSize}
+	renderer.Copy(ss, PC.Action[PC.Pose], &pos)
 
-	for _, obt := range Obstacles {
-		pos = &sdl.Rect{obt.Position.X, obt.Position.Y, tileSize, tileSize}
-		renderer.Copy(ts, obt.Source, pos)
+	for _, exp := range Explosions {
+		scrPoint := worldToScreen(exp.Position, Cam)
+		pos := sdl.Rect{scrPoint.X, scrPoint.Y, tileSize, tileSize}
+		renderer.Copy(ss, exp.Action[exp.Pose], &pos)
+
 	}
-
-	for _, exps := range Explosions {
-		pos = &sdl.Rect{exps.Position.X, exps.Position.Y, tileSize, tileSize}
-		renderer.Copy(ss, exps.Action[exps.Pose], pos)
-	}
-
-	pos = &sdl.Rect{PC.Position.X, PC.Position.Y, tileSize, tileSize}
-	renderer.Copy(ss, PC.Action[PC.Pose], pos)
 
 	renderer.Present()
 }
@@ -228,10 +263,12 @@ func main() {
 
 	renderer.SetDrawColor(0, 0, 255, 255)
 
-	for i := 0; i < 20; i++ {
-		cX := rand.Int31n(int32(winWidth / tileSize))
-		cY := rand.Int31n(int32(winHeight / tileSize))
+	buildDummyWorld()
 
+	for i := 0; i < 2020; i++ {
+		cX := rand.Int31n(WORLD_CELLS_X)
+		cY := rand.Int31n(WORLD_CELLS_Y)
+		World[cX][cY] = WALL
 		Obstacles = append(Obstacles, &StillObj{
 			Position:  Vector2d{cX * tileSize, cY * tileSize},
 			Source:    WALL,
@@ -239,12 +276,9 @@ func main() {
 		})
 	}
 
-	cellsX := int32(winWidth / tileSize)
-	cellsY := int32(winHeight / tileSize)
-	for i := 0; i < 8; i++ {
-
-		cX := rand.Int31n(cellsX)
-		cY := rand.Int31n(cellsY)
+	for i := 0; i < 800; i++ {
+		cX := rand.Int31n(WORLD_CELLS_X)
+		cY := rand.Int31n(WORLD_CELLS_Y)
 
 		Explosions = append(Explosions, &AnimatedObj{
 			Position: Vector2d{cX * tileSize, cY * tileSize},
@@ -263,5 +297,19 @@ func main() {
 		renderScene(renderer, tilesetTxt, spritesheetTxt)
 		tick1 = tick2
 		sdl.Delay(33)
+	}
+}
+
+func buildDummyWorld() {
+	for i := 0; i < WORLD_CELLS_X; i++ {
+		for j := 0; j < WORLD_CELLS_Y; j++ {
+			tile := GRASS
+
+			if rand.Int31n(10) < 1 {
+				tile = TREE
+			}
+
+			World[i][j] = tile
+		}
 	}
 }
